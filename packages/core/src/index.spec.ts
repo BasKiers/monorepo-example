130a1a75fdf80bbfff8cce5321b2f61664048245
Bas Kiers <kiers.bas@gmail.com>
2020-09-28 15:36:19 +0200

    add test

--- packages/core/src/index.spec.ts
+++ packages/core/src/index.spec.ts
@@ -1,5 +1,11 @@
+import {sum} from "./index";
+
 describe('it', () => {
   it('dummy', () => {
     expect(true).toEqual(true)
+  })
+
+  it('check sum', () => {
+    expect(sum(5, 10)).toBe(15)
   })
 });
